22eacb5e9052bb0d9ed6b2cf587776a645bcac8d
dendims <96282318+dims03@users.noreply.github.com>
2023-08-11 09:00:05 +0800

    Update script.js
    add js for smooth scrolling

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,3 +20,30 @@ window.onscroll = function () {
     header.classList.remove("navbar-fixed");
   }
 };
+
+// Smooth scrolling
+
+// Select all navigation links that should trigger smooth scroll
+const navLinks = document.querySelectorAll('nav a[href^="#home"]');
+const aboutLinks = document.querySelectorAll('nav a[href^="#about"]');
+const portfolioLinks = document.querySelectorAll('nav a[href^="#portfolio"]');
+const contactLinks = document.querySelectorAll('nav a[href^="#contact"]');
+
+// Combine all navigation links into a single array
+const allNavLinks = [...navLinks, ...aboutLinks, ...portfolioLinks, ...contactLinks];
+
+// Add click event listener to each navigation link
+allNavLinks.forEach((navLink) => {
+  navLink.addEventListener("click", function (e) {
+    e.preventDefault(); // Prevent default link behavior
+
+    const target = document.querySelector(this.getAttribute("href")); // Get the target section
+
+    if (target) {
+      // Scroll to the target section smoothly
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
